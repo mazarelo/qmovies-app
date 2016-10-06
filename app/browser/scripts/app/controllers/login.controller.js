4303aa03f574e0d3cf939b1350daf65f90322c5d
Joao <info@mazarelo.com>
2016-10-06 22:51:18 +0100

    no message

---
+++ app/browser/scripts/app/controllers/login.controller.js
@@ -0,0 +1,4 @@
+myApp.controller("LoginController" , function( $scope, database ) {
+  const self = this;
+  self.pageName = "Login";
+});
--- app/browser/scripts/app/routes.js
+++ app/browser/scripts/app/routes.js
@@ -3,27 +3,59 @@ myApp.config(function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
     templateUrl: 'views/main.html',
     replace: true,
-    controller:'MainController as intro'
+    controller:'MainController as intro',
+    resolve: {
+
+    }
+  })
+  .when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'LoginController as login',
+    resolve: {
+      
+    }
+  })
+  .when('/register', {
+    templateUrl: 'views/register.html',
+    controller: 'RegisterController as register',
+    resolve: {
+
+    }
   })
   .when('/tv', {
     templateUrl: 'views/tv-alternative.html',
-    controller: 'TvController as tvFeed'
+    controller: 'TvController as tvFeed',
+    resolve: {
+
+    }
   })
   .when('/tv/:tvId/season/:season', {
     templateUrl: 'views/tv-main.html',
-    controller: 'TvController as tv'
+    controller: 'TvController as tv',
+    resolve: {
+
+    }
   })
   .when('/tv/:tvId/season/:season/episode/:episode', {
     templateUrl: 'views/tv-individual.html',
-    controller: 'TvController as tv'
+    controller: 'TvController as tv',
+    resolve: {
+
+    }
   })
   .when('/movies', {
     templateUrl: 'views/movies.html',
-    controller: 'MoviesController as movieFeed'
+    controller: 'MoviesController as movieFeed',
+    resolve: {
+
+    }
   })
   .when('/movies/:movieId', {
     templateUrl: 'views/movies-individual.html',
-    controller: 'MoviesController as movie'
+    controller: 'MoviesController as movie',
+    resolve: {
+
+    }
   })
   .otherwise({
     redirectTo: "/"
